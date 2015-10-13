654e344de0b1a650124079fc35dc44bce039ae4e
Joshua Pedrick <Joshua.Pedrick@gmail.com>
2015-10-13 17:28:02 -0400

    finalized tmux configuration

--- install.sh
+++ install.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
+set -e
+me=`basename $0`
+
+function error_exit
+{
+    local parent_lineno="$1"
+    local message="$2"
+    local code="${3:-1}"
+    if [[ -n "$message" ]] ; then
+        echo "Error on or near line ${parent_lineno}: ${message}; exiting with status ${code}"
+    else
+        echo "Error on or near line ${parent_lineno}; exiting with status ${code}"
+    fi
+    exit "${code}"
+}
+
+trap 'error_exit ${LINENO}' ERR
+
 mkdir -p ~/.vim/bundle
 cp .vim/vim_plugins ~/.vim/
 
@@ -12,6 +30,13 @@ popd
 cp .Xdefaults ~/
 xrdb ~/.Xdefaults
 
-cd ~/.vim/bundle/YouCompleteMe
+pushd ~/.vim/bundle/YouCompleteMe
 ./install.py --clang-completer
+popd
+
+sudo apt-get install tmux
+
+mkdir -p ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+cp .tmux.conf ~/
 
