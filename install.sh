421ca1afb44b68acc55d884e80aef737ced2e017
Joshua Pedrick <Joshua.Pedrick@gmail.com>
2015-11-04 10:16:12 -0500

    mark progress of install

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+set -ev
 me=`basename $0`
 
 function error_exit
@@ -15,42 +15,118 @@ function error_exit
     exit "${code}"
 }
 
+function on_exit(){
+    echo $PROGRESS > $PROGRESS_FILE
+}
+
 trap 'error_exit ${LINENO}' ERR
+trap 'on_exit ' EXIT
+
+
+PROGRESS_FILE=env_install_progress.txt
+if [ -f $PROGRESS_FILE ]; then LOADED_PROGRESS=$(<${PROGRESS_FILE}); fi
+PROGRESS=${LOADED_PROGRESS:-0}
+
+STEP=0
+
+if (( PROGRESS < STEP )); then
 
 mkdir -p ~/.vim/bundle
-cp .vim/vim_plugins ~/.vim/
+cp -P .vim/vim_plugins ~/.vim/
+pushd ~/
+ln -s .vim/vimrc ~/.vimrc
+ln -s .vim .config/nvim
+popd
 
+((PROGRESS+=1))
+fi
+((STEP+=1))
+
+if (( PROGRESS < STEP )); then
+
+echo Install Nvim
+git clone https://github.com/neovim/neovim.git
+make
+sudo make install
+
+((PROGRESS+=1))
+fi
+((STEP+=1))
+
+if (( PROGRESS < STEP )); then
 echo Install Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim -c 'PluginInstall' -c 'q' 
 vim -c 'so %' -c 'q' Align.vba 
+
+((PROGRESS+=1))
+fi
+((STEP+=1))
+
+if (( PROGRESS < STEP )); then
 git clone https://github.com/powerline/fonts.git powerline-fonts
 pushd powerline-fonts
 ./install.sh
 popd
+
+((PROGRESS+=1))
+fi
+((STEP+=1))
+
+
+if (( PROGRESS < STEP )); then
 cp .Xdefaults ~/
 xrdb ~/.Xdefaults
 
+((PROGRESS+=1))
+fi
+((STEP+=1))
+
+if (( PROGRESS < STEP )); then
 echo compile YouCompleteMe with clang-completer
 pushd ~/.vim/bundle/YouCompleteMe
 ./install.py --clang-completer
 popd
 
+((PROGRESS+=1))
+fi
+((STEP+=1))
+
+if (( PROGRESS < STEP )); then
 echo Installing tmux
 sudo apt-get install tmux
 
+((PROGRESS+=1))
+fi
+((STEP+=1))
+
+if (( PROGRESS < STEP )); then
 echo Installing tmux package manager
 mkdir -p ~/.tmux/plugins/tpm
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 cp .tmux.conf ~/
 
+((PROGRESS+=1))
+fi
+((STEP+=1))
+
+if (( PROGRESS < STEP )); then
 echo Upgrading git
 sudo add-apt-repository ppa:git-core/ppa
 sudo apt-get update
 sudo apt-get install git
 git --version
 
+((PROGRESS+=1))
+fi
+((STEP+=1))
+
+if (( PROGRESS < STEP )); then
 echo Installing git WebUI
 git clone https://github.com/alberthier/git-webui.git
 git config --global alias.webui \!$PWD/git-webui/release/libexec/git-core/git-webui
+
+((PROGRESS+=1))
+fi
+((STEP+=1))
 
